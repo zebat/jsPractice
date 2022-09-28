504c1f54e260d111f15eaa49f56f1f99b2b0be45
Zeba <50170893+zeba-tahreen@users.noreply.github.com>
2022-09-28 11:49:31 -0400

    Initial files

---
+++ variables_Ops.js
@@ -0,0 +1,9 @@
+function var_ops_1(){
+    var a = parseInt(prompt('Enter First Number'));
+    var b = parseInt(prompt('Enter Second Number'));   
+    var c = a + b;
+    var d = a * b;
+   alert("Addition of " + a + " + " + b + " = " + c);
+   alert("Multiplication of " + a + " * " + b + " = " + d);
+   document.getElementById("var_ops_li_pre1").style.display = 'block';
+};
